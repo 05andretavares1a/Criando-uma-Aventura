2131945370a2b5cb111c6ad894b98e8df58d9081
05andretavares1a <andre.tavares.nascimento@escola.pr.gov.br>
2025-10-16 10:52:49 -0300

    s

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const avanca = document.querySelectorALL('.bnt-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getAttribute('data-proxima');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
